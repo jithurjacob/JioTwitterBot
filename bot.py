91386c830a3dbc5c3cff5650e9d6cf37c84be427
Jithu R Jacob <jithurjacob@gmail.com>
2016-10-21 20:15:38 +0530

    fixed the bug of sending old image

--- bot.py
+++ bot.py
@@ -5,23 +5,7 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
 
-font = ImageFont.truetype("Aaargh.ttf",40) #https://www.fontsquirrel.com/fonts/download/Aaargh
-jio_date = datetime.strptime("2-10-2016 14:00","%d-%m-%Y %H:%M")
-time_diff = (datetime.now() - jio_date ).total_seconds()
-num_days = int(time_diff/ (24*3600))
-num_hours = int((time_diff - (num_days*24*3600))/3600)
-num_minutes = int(((time_diff - (num_days*24*3600))%3600)/60)
-text = "It's been {} days, {} hours, {} minutes\nsince I got my SIM, it's still not activated.".format(num_days, num_hours, num_minutes)
-tcolor = (0,0,0)
-text_pos = (100,100)
 
-img = Image.open("jio_image.jpg")
-img = img.convert('RGB')
-draw = ImageDraw.Draw(img)
-draw.text(text_pos, text, fill=tcolor, font=font)
-del draw
-
-img.save("jio_output.jpg")
 
 
 
@@ -34,10 +18,28 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-api.update_with_media("jio_output.jpg", status= "@JioCare I'm done with this... ")# for AWS lamda scheduler
-#while True:
-#    #api.update_status("")
-#    #api.update_with_media("jio_output.jpg", status= "@JioCare I'm done with this... ")
-#    print("Tweeted now ",datetime.now().strftime("%d-%m-%Y %H:%M"))
-#    time.sleep(900)#Tweet every 15 minutes
+def lambda_handler():
+	api.update_with_media("jio_output.jpg", status= "@JioCare I'm done with this... https://github.com/jithurjacob/JioTwitterBot")# for AWS lamda scheduler
+	print("done")
+while True:
+	font = ImageFont.truetype("Aaargh.ttf",40) #https://www.fontsquirrel.com/fonts/download/Aaargh
+	jio_date = datetime.strptime("2-10-2016 14:00","%d-%m-%Y %H:%M")
+	time_diff = (datetime.now() - jio_date ).total_seconds()
+	num_days = int(time_diff/ (24*3600))
+	num_hours = int((time_diff - (num_days*24*3600))/3600)
+	num_minutes = int(((time_diff - (num_days*24*3600))%3600)/60)
+	text = "It's been {} days, {} hours, {} minutes\nsince I got my SIM, it's still not activated.".format(num_days, num_hours, num_minutes)
+	tcolor = (0,0,0)
+	text_pos = (100,100)
+
+	img = Image.open("jio_image.jpg")
+	img = img.convert('RGB')
+	draw = ImageDraw.Draw(img)
+	draw.text(text_pos, text, fill=tcolor, font=font)
+	del draw
+
+	img.save("jio_output.jpg")#api.update_status("")
+	api.update_with_media("jio_output.jpg", status= "@JioCare I'm done with this... https://github.com/jithurjacob/JioTwitterBot")
+	print("Tweeted now ",datetime.now().strftime("%d-%m-%Y %H:%M"))
+	time.sleep(900)#Tweet every 15 minutes
 
