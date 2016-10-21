09b64526e1fe09aafe956e6f125db693db242766
Jithu R Jacob <jithurjacob@gmail.com>
2016-10-21 16:56:36 +0530

    initial commit
    initial

---
+++ bot.py
@@ -0,0 +1,43 @@
+import tweepy, time, sys
+from keys import keys
+from datetime import datetime, date
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+
+font = ImageFont.truetype("Aaargh.ttf",40) #https://www.fontsquirrel.com/fonts/download/Aaargh
+jio_date = datetime.strptime("2-10-2016 14:00","%d-%m-%Y %H:%M")
+time_diff = (datetime.now() - jio_date ).total_seconds()
+num_days = int(time_diff/ (24*3600))
+num_hours = int((time_diff - (num_days*24*3600))/3600)
+num_minutes = int(((time_diff - (num_days*24*3600))%3600)/60)
+text = "It's been {} days, {} hours, {} minutes\nsince I got my SIM, it's still not activated.".format(num_days, num_hours, num_minutes)
+tcolor = (0,0,0)
+text_pos = (100,100)
+
+img = Image.open("jio_image.jpg")
+img = img.convert('RGB')
+draw = ImageDraw.Draw(img)
+draw.text(text_pos, text, fill=tcolor, font=font)
+del draw
+
+img.save("jio_output.jpg")
+
+
+
+#enter the corresponding information from your Twitter application:
+CONSUMER_KEY = keys["CONSUMER_KEY"]
+CONSUMER_SECRET = keys["CONSUMER_SECRET"]
+ACCESS_KEY = keys["ACCESS_KEY"]
+ACCESS_SECRET = keys["ACCESS_SECRET"]
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+api.update_with_media("jio_output.jpg", status= "@JioCare I'm done with this... ")# for AWS lamda scheduler
+#while True:
+#    #api.update_status("")
+#    #api.update_with_media("jio_output.jpg", status= "@JioCare I'm done with this... ")
+#    print("Tweeted now ",datetime.now().strftime("%d-%m-%Y %H:%M"))
+#    time.sleep(900)#Tweet every 15 minutes
+
